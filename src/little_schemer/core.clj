2084160c8e9f7b420b28fce6c952b082b84d8d68
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-21 20:38:12 -0500

    core/atom? and core/lat?

--- src/little_schemer/core.clj
+++ src/little_schemer/core.clj
@@ -1,1 +1,13 @@
 (ns little-schemer.core)
+
+(defn atom?
+  [a]
+  (not (list? a)))
+
+(defn lat?
+  [lat]
+  (cond
+    (atom? lat) false
+    (empty? lat) true
+    (list? (first lat)) false
+    true (lat? (rest lat))))
--- test/little_schemer/test/core.clj
+++ test/little_schemer/test/core.clj
@@ -2,5 +2,14 @@
   (:use [little-schemer.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest atom?-test
+  (is (= true (atom? 'a)))
+  (is (= true (atom? :b)))
+  (is (= true (atom? 2)))
+  (is (= false (atom? '(a)))))
+
+(deftest lat?-test
+  (is (= true (lat? ())))
+  (is (= true (lat? '(a b c d e))))
+  (is (= false (lat? 'a)))
+  (is (= false (lat? '(a (b) c d e)))))
