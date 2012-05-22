c0fc644cbf02f41e666ad01de3e68112422d6265
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-21 19:40:18 -0500

    Initial commit.

---
+++ src/little_schemer/core.clj
@@ -0,0 +1,1 @@
+(ns little-schemer.core)
---
+++ test/little_schemer/test/core.clj
@@ -0,0 +1,6 @@
+(ns little-schemer.test.core
+  (:use [little-schemer.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
