6198ad12a64a1f92fae9df675d62b30f078f6dd8
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-05-21 20:03:15 -0500

    ch4/one? & ch4/rempick

--- src/little_schemer/ch4.clj
+++ src/little_schemer/ch4.clj
@@ -7,3 +7,14 @@
     (= (first lat) a) (+ 1 (occur a (rest lat)))
     ;(= (first lat) a) (+ 1 0)))
     true (occur a (rest lat))))
+
+(defn one?
+  [n]
+  (= n 1))
+
+(defn rempick
+  [n lat]
+  (cond
+    (empty? lat) ()
+    (= n 1) (rest lat)
+    true (cons (first lat) (rempick (- n 1) (rest lat)))))
--- test/little_schemer/test/ch4.clj
+++ test/little_schemer/test/ch4.clj
@@ -7,3 +7,15 @@
   (is (= 1 (occur 'b '(b 2 3))))
   (is (= 1 (occur 'c '(1 c 3))))
   (is (= 3 (occur 'd '(d d d)))))
+
+(deftest one?-test
+  (is (one? 1))
+  (is (not (one? 'a)))
+  (is (not (one? 0))))
+
+(deftest rempick-test
+  (let [lat '(lemon meringue salty pie)]
+    (is (= (rempick 1 lat) '(meringue salty pie)))
+    (is (= (rempick 2 lat) '(lemon salty pie)))
+    (is (= (rempick 3 lat) '(lemon meringue pie)))
+    (is (= (rempick 4 lat) '(lemon meringue salty)))))
